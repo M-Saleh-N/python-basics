d9ba75c2f08f57665ed5533a498eb4be309ffa78
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-07 17:14:48 +0300

    added a weather instrusction tracker

---
+++ weather.py
@@ -0,0 +1,19 @@
+print("Heyyy there , whats your score?")
+print("__________________________________________")
+
+
+weather = str(input("what's the weather likr today?: "))
+
+if weather == "sunny":
+    print("Wear sunglasses!😎")
+
+elif weather == "rainy":
+    print("Take an umbrella!☔")
+
+elif weather == "cold":
+    print("Wear a jecket!🧥")
+
+else:
+    print("Stay safe out there!")
+
+    
