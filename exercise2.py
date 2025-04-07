f4ce3b172613dcf661bc8b20a4097b96bcde7124
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-07 17:09:02 +0300

    added a score task

---
+++ exercise2.py
@@ -0,0 +1,19 @@
+print("Heyyy there , whats your score?")
+print("__________________________________________")
+
+score = int(input("Enter your score: "))
+
+if score >= 90:
+    print("Grade: A😍")
+
+elif score >= 80:
+    print("Grade: B👍")
+
+elif score >= 70:
+    print("Grade: C😊")
+    
+elif score >= 60:
+    print("Grade: D😑")
+
+else:
+    print("Failure🤣")
