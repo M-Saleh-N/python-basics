4d25b4cb871279519cb75b143980fe109aedad7b
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-07 16:59:20 +0300

    added more animals

--- exercise.py
+++ exercise.py
@@ -4,4 +4,11 @@ animal = str(input("Enter your fav animal: "))
 
 if animal == "dog":
     print("Dogs are loyal and awesome")
+elif animal == "cat":
+    print("Cats are cute and love naps")
+elif animal == "lion":
+    print("Lions are the kings of the jungle")
+else: 
+    print("That's a cool animal")
+
 
