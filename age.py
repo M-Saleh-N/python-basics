ad2fa68d2a8bbfcf7f14c185637be3433765bf1b
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-07 17:18:53 +0300

    created an age tracker

---
+++ age.py
@@ -0,0 +1,12 @@
+print("Heyyy there?")
+print("__________________________________________")
+age = int(input("what's your age?: "))
+
+if age <= 13:
+    print("You are a kid!")
+
+elif age >= 14:
+    print("You're a teenager!")
+
+elif age >= 20:
+    print("You're an adult!")
