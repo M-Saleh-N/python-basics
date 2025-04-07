fc0347de9e48d250e0bcad08727371abd1739cd4
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-07 17:33:23 +0300

    created a list

---
+++ list.py
@@ -0,0 +1,14 @@
+print("Heyyy there?")
+print("__________________________________________")
+
+number_x = [10, 20, 30, 40, 10]
+number_y = [75, 65, 35, 75, 30]
+
+first = number_x(0)
+second = number_y(-1)
+
+if first == second:
+    print("The first and last number are the same ")
+
+else:
+    print("The first and last number are not the same")
