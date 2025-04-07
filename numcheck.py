96339673f38f14a61ae80a6f1ea506158cac0851
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-07 17:47:33 +0300

    created a number check +ve or -ve

---
+++ numcheck.py
@@ -0,0 +1,14 @@
+num =  int(input("Enter a number: "))
+
+if num > 0:
+    print("positive")
+
+elif num < 0:
+    print("Negative")
+
+elif num == 0:
+    print("zero")
+
+else:
+    print("thank you")
+
