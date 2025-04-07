4177dca4baf6ebb370951f80e5fb04d64e3c7245
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-07 16:48:44 +0300

    added

--- main.py
+++ main.py
@@ -26,3 +26,26 @@ f = False
 fruits = ["apple", "banana"]
 fruits.append("orange")
 
+#Tuple (Ordered, immutable)
+coords = (10,20)
+
+#Dictonary (Key:Value pairs, Mutable)
+person = {"name": "Vansh", "age": 17}
+
+#Sets (Unordered, Unique elements)
+colors = {"red", "green", "blue"}
+colors.add("yellow")
+
+#Type Conversion
+#Converting from one data type to another 
+#Converting int to str
+num = 100.12
+numStr = str(num) # "100.12"
+
+#Converting str to int
+numInt = int(numStr) # 100.12
+
+#Converting str to float
+numFloat = float(numStr) # 100.12
+
+print(numInt)
