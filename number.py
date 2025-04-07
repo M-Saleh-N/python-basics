90e5128b513c15d22af3b3342bbe9d4037007549
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-07 17:27:53 +0300

    addded a calc

---
+++ number.py
@@ -0,0 +1,14 @@
+print("Heyyy there?")
+print("__________________________________________")
+
+sum = 0
+
+number1 = int(input("Enter your first number: "))
+number2 = int(input("Enter your second number: "))
+
+if number1 * number2  > 1000:
+    sum = number1 + number2
+    print(sum)
+
+else:
+    print("Thank you for trying this code")
